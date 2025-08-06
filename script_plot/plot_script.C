7ff922e57036b36b82619d7022a9f94202856981
boaca926-beep <caobo875@gamil.com>
2025-08-06 09:53:13 +0200

    all changes

--- script_plot/plot_script.C
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-void plot_script() {
-  gROOT->ProcessLine(".L ../run_plot/plot_omega.C");
-  gROOT->ProcessLine("plot_omega()");
-}
--- script_syst/tree_cut_script.C
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-void tree_cut_script() {
-gROOT->ProcessLine(".L ../run/tree_cut.C");
-gROOT->ProcessLine("tree_cut()");
-}

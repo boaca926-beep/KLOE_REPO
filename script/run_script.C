c7df037c71459c5b8702e35d8826487b6a8e584c
boaca926-beep <caobo875@gamil.com>
2025-09-05 08:47:04 +0200

    all changes

--- script/run_script.C
+++
@@ -1,5 +0,0 @@
-void run_script() {
-  gROOT->ProcessLine(".L ../run_vertex_bkgrej/MyClass.C");
-  gROOT->ProcessLine(".L ../run/Analys_class.C");
-  gROOT->ProcessLine("Analys_class(rootFile, sampleFile)");
-}

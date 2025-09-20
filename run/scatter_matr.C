0c9efd1a4e996dde37bb1df2aee9422bd04b7f96
boaca926-beep <caobo875@gamil.com>
2025-09-20 09:34:03 +0200

    all changes

--- run/scatter_matr.C
+++ run/scatter_matr.C
@@ -2,15 +2,16 @@
 //#include "crx3pi.h"
 #include "../header/sm_para.h"
 
-const double fit_min = 732;
-const double fit_max = 832;
+const double fit_min = 760;
+const double fit_max = 800;
 
 const double IM3pi_min = 380; //
 const double IM3pi_max = 1020; //
 
 const TString hist_nm = "_corred";
 //const TString infile_nm = "/home/bo/Desktop/analysis/crx3pi/output_norm/crx3pi0.root";
-const TString infile_nm = "/home/bo/Desktop/analysis_root_v6/input_vertex_TDATA_bkgrej";
+//const TString infile_nm = "/home/bo/Desktop/analysis_root_v6/input_vertex_TDATA_bkgrej";
+const TString infile_nm = "/home/bo/Desktop/input_norm_TDATA";
 TFile *infile = new TFile(infile_nm + "/cut/tree_pre.root");
 TFile *inputfile = new TFile(infile_nm + "/input/sig.root"); 
 TFile *f_hist = new TFile(infile_nm + "/hist/hist.root"); 
@@ -19,7 +20,8 @@ TH1D *hdata = (TH1D *)HIM3pi_fit -> FindObject("h1d_IM3pi_TDATA") -> Clone();
 //hdata -> Draw();
 
 TH2D *hcorrmatrix_tuned;
-TH2D *hcorrmatrix_good;
+TH2D *hcorrmatrix_good;  
+
 
 TRandom *generator = new TRandom();
 TRandom *rnd=0;
@@ -85,20 +87,17 @@ TH2D *getCorrMatrix_tuned(int bins, double xmin, double xmax) {
 }
 
 //
-TH2D *getCorrMatrix_good(int bins, double xmin, double xmax) {// good signal events
+void getCorrMatrix_good(TH2D *h2d_good, TH2D *h2d_bad) {// good signal events
 
   TTree *ALLCHAIN_CUT = (TTree*)inputfile -> Get("ALLCHAIN_CUT");
 
-  // create corrlation matrix
-  TH2D *h2d_corr = new TH2D("", "", bins, xmin, xmax, bins, xmin, xmax);
-  
   //cout << "correlation matrix is created!!! " << TISR3PI_SIG -> GetName() << endl;
 
   int phid = 9999;
   int bkg_indx = 9999;
   int sig_type = 9999;
   int recon_indx = 9999;
-
+  
   double IM3pi = 0., IM3pi_true = 0.;
   double IM3pi_corred = 0.;
   double PARA[5] = {frac, smallBias, smallSigma, wideBias, wideSigma};
@@ -115,7 +114,7 @@ TH2D *getCorrMatrix_good(int bins, double xmin, double xmax) {// good signal eve
     IM3pi_true = ALLCHAIN_CUT -> GetLeaf("Br_IM3pi_true") -> GetValue(0);
     IM3pi = ALLCHAIN_CUT -> GetLeaf("Br_IM3pi_7C") -> GetValue(0);
     
-    //if (irow > 1e5) break;
+    //if (irow > 1e6) break;
 
     //h2d_corr -> Fill(IM3pi_true, IM3pi);
 	
@@ -123,15 +122,16 @@ TH2D *getCorrMatrix_good(int bins, double xmin, double xmax) {// good signal eve
       
       //hist -> Fill(IM3pi);
       //h2d_scatter -> Fill(IM3pi_true, IM3pi);
-      
+
+      //if (recon_indx == 2) {
       if (recon_indx == 2 && bkg_indx == 1) {
-	h2d_corr -> Fill(IM3pi_true, IM3pi);
+	h2d_good -> Fill(IM3pi_true, IM3pi);
 	//cout << IM3pi_true << ", " << IM3pi << endl;
 	
 	//h1d_resol_Eisr -> Fill(Eisr_resol);
       }
       else {
-	//h2d_corr -> Fill(IM3pi_true, IM3pi);
+	h2d_bad -> Fill(IM3pi_true, IM3pi);
 	
 	//hist_wrong -> Fill(IM3pi);
 	//h2d_scatter_wrong -> Fill(IM3pi_true, IM3pi);
@@ -143,7 +143,7 @@ TH2D *getCorrMatrix_good(int bins, double xmin, double xmax) {// good signal eve
     
   }
   
-  return h2d_corr;
+  //return h2d_corr;
   
 }
 
@@ -178,25 +178,30 @@ int scatter_matr() {
 
   int binsize = hdata -> GetNbinsX();
   hcorrmatrix_tuned = getCorrMatrix_tuned(binsize, IM3pi_min, IM3pi_max);
-  hcorrmatrix_good = getCorrMatrix_good(binsize, IM3pi_min, IM3pi_max);
-  hcorrmatrix_good -> Draw();
+
+  TH2D *hcorrmatrix_good = new TH2D("hcorrmatrix_good", "", binsize, IM3pi_min, IM3pi_max, binsize, IM3pi_min, IM3pi_max);
+  TH2D *hcorrmatrix_bad = new TH2D("hcorrmatrix_bad", "", binsize, IM3pi_min, IM3pi_max, binsize, IM3pi_min, IM3pi_max);
+  
+  getCorrMatrix_good(hcorrmatrix_good, hcorrmatrix_bad);
+  //hcorrmatrix_good -> Draw();
   //hcorrmatrix_tuned -> Draw();
 
   /*
   //TH2D * h2d = (TH2D*)infile -> Get("h2d_scatter" + hist_nm); // smeared matrix
   TH2D * h2d = (TH2D*)infile -> Get("h2d_scatter");  // reconstructed matrix
-
-  double norm_factor = h2d -> Integral();
-  //h2d -> Scale(1. / norm_factor);
-  cout << norm_factor << ", h2d:" << h2d -> GetName() << endl;
+  */
+  
+  double norm_factor = hcorrmatrix_tuned -> Integral();
+  //hcorrmatrix_tuned -> Scale(1. / norm_factor);
+  cout << norm_factor << ", hcorrmatrix_tuned:" << hcorrmatrix_tuned -> GetName() << endl;
   
   // plot
 
-  TLine *line1 = new TLine(fit_min, IM3pi_max, fit_max, IM3pi_max); //upper horiz
+  TLine *line1 = new TLine(IM3pi_min, fit_max, IM3pi_max, fit_max); //upper horiz
   line1 -> SetLineColor(kBlack);
   line1 -> SetLineWidth(3);
 
-  TLine *line2 = new TLine(fit_min, IM3pi_min, fit_max, IM3pi_min); //lower horiz
+  TLine *line2 = new TLine(IM3pi_min, fit_min, IM3pi_max, fit_min); //lower horiz
   line2 -> SetLineColor(kBlack);
   line2 -> SetLineWidth(3);
 
@@ -208,9 +213,10 @@ int scatter_matr() {
   line4 -> SetLineColor(kBlack);
   line4 -> SetLineWidth(3);
 
-  double entries = h2d -> GetEntries();
-  
-  TCanvas * cv = new TCanvas("cv", "scatter" + hist_nm, 0, 0, 700, 700);
+  double entries = hcorrmatrix_tuned -> GetEntries();
+
+  ////
+  TCanvas * cv = new TCanvas("cv", "corrmatrix_tuned", 0, 0, 700, 700);
   cv -> SetBottomMargin(0.15);
   cv -> SetLeftMargin(0.15);
   cv -> SetRightMargin(0.15);
@@ -229,42 +235,78 @@ int scatter_matr() {
 
   //const double axis_min = 550., axis_max = 900.;
   
-  h2d -> GetZaxis() -> SetLabelSize(0.045);
+  hcorrmatrix_tuned -> GetZaxis() -> SetLabelSize(0.045);
 
-  h2d -> GetXaxis() -> CenterTitle();
-  h2d -> GetXaxis() -> SetTitle("M^{true}_{3#pi} [MeV/c^{2}]");
-  h2d -> GetXaxis() -> SetTitleOffset(1.0);
-  h2d -> GetXaxis() -> SetTitleSize(0.06);
-  h2d -> GetXaxis() -> SetLabelSize(0.045);
-  //h2d -> GetXaxis() -> SetRangeUser(axis_min, axis_max);
+  hcorrmatrix_tuned -> GetXaxis() -> CenterTitle();
+  hcorrmatrix_tuned -> GetXaxis() -> SetTitle("M^{true}_{3#pi} [MeV/c^{2}]");
+  hcorrmatrix_tuned -> GetXaxis() -> SetTitleOffset(1.0);
+  hcorrmatrix_tuned -> GetXaxis() -> SetTitleSize(0.06);
+  hcorrmatrix_tuned -> GetXaxis() -> SetLabelSize(0.045);
+  //hcorrmatrix_tuned -> GetXaxis() -> SetRangeUser(axis_min, axis_max);
   
-  h2d -> GetYaxis() -> CenterTitle();
-  h2d -> GetYaxis() -> SetTitle("M^{rec}_{3#pi} [MeV/c^{2}]");
-  //h2d -> GetYaxis() -> SetTitle("M^{smear}_{3#pi} [MeV/c^{2}]");
-  h2d -> GetYaxis() -> SetTitleOffset(1.2);
-  h2d -> GetYaxis() -> SetTitleSize(0.06);
-  h2d -> GetYaxis() -> SetLabelSize(0.045);
-  //h2d -> GetYaxis() -> SetRangeUser(axis_min, axis_max);
+  hcorrmatrix_tuned -> GetYaxis() -> CenterTitle();
+  hcorrmatrix_tuned -> GetYaxis() -> SetTitle("M^{rec}_{3#pi} [MeV/c^{2}]");
+  //hcorrmatrix_tuned -> GetYaxis() -> SetTitle("M^{smear}_{3#pi} [MeV/c^{2}]");
+  hcorrmatrix_tuned -> GetYaxis() -> SetTitleOffset(1.2);
+  hcorrmatrix_tuned -> GetYaxis() -> SetTitleSize(0.06);
+  hcorrmatrix_tuned -> GetYaxis() -> SetLabelSize(0.045);
+  //hcorrmatrix_tuned -> GetYaxis() -> SetRangeUser(axis_min, axis_max);
   
-  //h2d -> Draw("TEXT0COL");
-  h2d -> Draw("COLZ");
+  //hcorrmatrix_tuned -> Draw("TEXT0COL");
+  hcorrmatrix_tuned -> Draw("COLZ");
   //pt1 -> Draw("Same");
-  //line1 -> Draw("Same");
-  //line2 -> Draw("Same");
-  line3 -> Draw("Same");
-  line4 -> Draw("Same");
+  line1 -> Draw("Same");
+  line2 -> Draw("Same");
+  //line3 -> Draw("Same");
+  //line4 -> Draw("Same");
 
   gPad -> SetLogz();
+
+  ////
+  TCanvas * cv1 = new TCanvas("cv1", "corrmatrix_tuned", 0, 0, 700, 700);
+  cv1 -> SetBottomMargin(0.15);
+  cv1 -> SetLeftMargin(0.15);
+  cv1 -> SetRightMargin(0.15);
+  
+  //const double axis_min = 550., axis_max = 900.;
+  
+  hcorrmatrix_good -> GetZaxis() -> SetLabelSize(0.045);
+
+  hcorrmatrix_good -> GetXaxis() -> CenterTitle();
+  hcorrmatrix_good -> GetXaxis() -> SetTitle("M^{true}_{3#pi} [MeV/c^{2}]");
+  hcorrmatrix_good -> GetXaxis() -> SetTitleOffset(1.0);
+  hcorrmatrix_good -> GetXaxis() -> SetTitleSize(0.06);
+  hcorrmatrix_good -> GetXaxis() -> SetLabelSize(0.045);
+  //hcorrmatrix_good -> GetXaxis() -> SetRangeUser(axis_min, axis_max);
+  
+  hcorrmatrix_good -> GetYaxis() -> CenterTitle();
+  hcorrmatrix_good -> GetYaxis() -> SetTitle("M^{rec}_{3#pi} [MeV/c^{2}]");
+  //hcorrmatrix_good -> GetYaxis() -> SetTitle("M^{smear}_{3#pi} [MeV/c^{2}]");
+  hcorrmatrix_good -> GetYaxis() -> SetTitleOffset(1.2);
+  hcorrmatrix_good -> GetYaxis() -> SetTitleSize(0.06);
+  hcorrmatrix_good -> GetYaxis() -> SetLabelSize(0.045);
+  //hcorrmatrix_good -> GetYaxis() -> SetRangeUser(axis_min, axis_max);
+  
+  //hcorrmatrix_good -> Draw("TEXT0COL");
+  hcorrmatrix_good -> Draw("COLZ");
+  //pt1 -> Draw("Same");
+  line1 -> Draw("Same");
+  line2 -> Draw("Same");
+  //line3 -> Draw("Same");
+  //line4 -> Draw("Same");
+
+  gPad -> SetLogz();
+  
   // save
   //TFile *fout = new TFile("./plots/smearmatr.root", "recreate");
 
   //cout << cv_nm << endl;
   //cv -> SaveAs("./plots/scatter" + hist_nm +  ".pdf");
-  cv -> SaveAs("./plots/scatter.pdf");
+  cv -> SaveAs("./corrmatrix_tunned.pdf");
+  cv1 -> SaveAs("./corrmatrix_good.pdf");
   
   //fout -> Close();
 
-  */  
   return 0;
 
 }

6e4461321eeeee0f61f78c624b55d1106a962026
boaca926-beep <caobo875@gamil.com>
2025-10-01 11:25:05 +0200

    all changes

--- script_plot/plot_omega.sh
+++ script_plot/plot_omega.sh
@@ -6,7 +6,7 @@
 #FILE_TYPE=("chain")
 #FILE_TYPE=("norm")
 #FILE_TYPE=("mini")
-FILE_TYPE=("vertex" "efficy_norm")
+FILE_TYPE=("vertex" "efficy_norm" "norm")
 #FILE_TYPE=("isrlumi_norm" "norm")
 #FILE_TYPE=("vmd_norm" "norm" "efficy_norm" "isrlumi_norm")
 

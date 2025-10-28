04f7b2b2035ace962a21b5e9eeb47de79c1371a6
boaca926 <bo.cao@physics.uu.se>
2025-10-28 16:25:49 +0100

    all changes

---
+++ header/plot_models_syst.h
@@ -0,0 +1,7 @@
+//const TString plot_name = "output_smallBias";
+const TString para_name = "Gam_omega";
+//const TString para_ytit = "B_{ee}B_{3#pi}[10^{-5}]";
+//const TString para_unit = "";
+//const TString cut_symb = "M^{bias}_{3#pi,inner}";
+//const TString cut_unit = "[MeV/c^{2}]";
+//const TString output_nm = "smallBias";

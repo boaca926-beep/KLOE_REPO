a2edb6c69b589915b6e2614e4061d42d205da6a0
boaca926 <bo.cao@physics.uu.se>
2026-03-02 22:12:53 +0100

    all changes

--- header/path_sample.h
+++ header/path_sample.h
@@ -1,3 +1,3 @@
-const TString data_type = "sig";
-const TString sampleFile = "../../kloe_sample/input/sig";
+const TString data_type = "ksl";
+const TString sampleFile = "../../kloe_sample/input/ksl";
 const TString outputCut = "../../";

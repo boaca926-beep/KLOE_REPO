f691b608788ded063bb7fb5b9fbc95256646de16
bo <caobo875@gmail.com>
2026-03-17 07:16:41 +0100

    all changes

--- header/path_sample.h
+++ header/path_sample.h
@@ -1,3 +1,3 @@
-const TString data_type = "ksl";
-const TString sampleFile = "../../kloe_sample/input/ksl";
-const TString outputCut = "../../";
+const TString data_type = "sig";
+const TString sampleFile = "../../input_vertex_TDATA_bkgrej/input/sig";
+const TString outputCut = "../../kloe_sample_chain.root";
--- script/hist_script.C
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-void hist_script() {
-gROOT->ProcessLine(".L ../run/gethist.C");
-gROOT->ProcessLine("gethist()");
-}
